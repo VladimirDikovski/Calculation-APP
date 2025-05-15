ff79827b3c8cf1a9fcb7c6d0afc22bcbe2fc1aff
Vladimir Dikovski <58719391+VladimirDikovski@users.noreply.github.com>
2025-05-15 15:59:20 +0300

    Add files via upload

---
+++ Calculation App/Application/JS/product.js
@@ -0,0 +1,8 @@
+export class Product {
+  constructor(idUser, nameProduct, datePurchase, priceProduct) {
+    this.idUser = idUser;
+    this.nameProduct = nameProduct;
+    this.datePurchase = datePurchase;
+    this.priceProduct = priceProduct;
+  }
+}
---
+++ Calculation App/js/config.js
@@ -0,0 +1,1 @@
+export const salary = 2200;
